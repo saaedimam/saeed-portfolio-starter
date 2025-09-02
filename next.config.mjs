b270527d3230a4313e33a66f98b90a87fd685051
ioriimasu <emu.imam.ei@gmail.com>
2025-09-02 07:02:44 +0600

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withContentlayer } from "next-contentlayer";
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -5,4 +7,4 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
